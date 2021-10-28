85160ee25e88f5d7798bde36e6eba45a32d2b330
leodbueno15 <leonardo.b.bueno.23@dartmouth.edu>
2021-10-28 11:32:13 -0400

    First commit to Allergy Alert project since inception.

---
+++ app/src/main/java/com/example/allergyalert/AddProfile.kt
@@ -0,0 +1,21 @@
+package com.example.allergyalert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class AddProfile : AppCompatActivity() {
+
+    lateinit var cancelButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_profile)
+
+        cancelButton = findViewById(R.id.cancel_profile)
+
+        cancelButton.setOnClickListener {
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/allergyalert/ui/home/HomeFragment.kt
@@ -0,0 +1,68 @@
+package com.example.allergyalert.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.allergyalert.AddProfile
+import com.example.allergyalert.R
+import com.example.allergyalert.databinding.FragmentHomeBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+    lateinit var profileList: ListView
+    lateinit var addProfileButton: FloatingActionButton
+
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
+        profileList = binding.profilesListView
+        val profileArray: ArrayList<String> = ArrayList()
+        profileArray.add("Sample Profile")
+        val arrayAdapter: ArrayAdapter<String>? = context?.let { ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, profileArray) }
+        profileList.adapter = arrayAdapter
+
+        addProfileButton = binding.addProfileButton
+        addProfileButton.setOnClickListener {
+            val intent = Intent(activity, AddProfile::class.java)
+            activity?.startActivity(intent)
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

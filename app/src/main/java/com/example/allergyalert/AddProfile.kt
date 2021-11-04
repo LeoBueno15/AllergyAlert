c5827bd1373ed4073233593168549441ab282d5f
HamzaMusana <f0034v7@dartmouth.edu>
2021-11-04 03:16:22 -0400

    Change add_profile UI

--- app/src/main/java/com/example/allergyalert/AddProfile.kt
+++ app/src/main/java/com/example/allergyalert/AddProfile.kt
@@ -3,6 +3,7 @@ package com.example.allergyalert
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class AddProfile : AppCompatActivity() {
 
